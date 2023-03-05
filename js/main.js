24eab08b4ce1813ea82241c7d964e204cefb5d4e
crazyrems <tauziac0remy+fork@gmail.com>
2023-03-06 00:12:12 +0100

    🎞 Load local video file

--- js/main.js
+++ js/main.js
@@ -2,3 +2,23 @@
 window.onbeforeunload = function() {
     return "";
 };
+
+var currentVideoFile = null;
+
+function loadLocalFile(event) {
+  var file = event.target.files[0];
+  var reader = new FileReader();
+  reader.onload = function(event) {
+    var data = event.target.result;
+    var video = document.getElementById("videoPlayer");
+    video.src = URL.createObjectURL(file);
+    video.load();
+    currentVideoFile = file;
+  };
+  reader.readAsDataURL(file);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("fileBrowser").addEventListener("change", loadLocalFile, false);
+});
+
